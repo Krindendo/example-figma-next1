7277bf04077ec1ddba8546c7ea6b879348689c28
Krindendo work <lollipopfunkyx9@gmail.com>
2024-08-02 00:15:25 +0200

    add

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -2,29 +2,26 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 import { siteConfig } from "@/config/site";
+import { Logo } from "./logo";
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer
       className={cn(
-        "w-full flex flex-col justify-center items-center px-3 py-14 gap-11 bg-[url('/images/footer-image1.png')] bg-center md:pt-36",
+        "flex flex-col justify-center  px-3 py-4  bg-[#ede5da]",
         className
       )}
     >
-      <p className="text-[#101212] text-center">© Copyright 2024</p>
-      <div className="flex justify-center items-center gap-3">
-        <a
-          href={siteConfig.links.instagram}
-          className="rounded-full border border-white/15 p-3"
-        >
-          <img src="/icons/linkedin.png" className="" />
-        </a>
-        <a
-          href={siteConfig.links.instagram}
-          className="rounded-full border border-white/15 p-3"
-        >
-          <img src="/icons/instagram.png" className="" />
-        </a>
+      <div className="container inline-flex justify-between items-center gap-11">
+        <Logo className="w-[150px]" />
+        <div className="flex justify-center items-center gap-3">
+          <a href={siteConfig.links.instagram} className="  p-3">
+            <img src="/icons/linkedin.png" className="" />
+          </a>
+          <a href={siteConfig.links.instagram} className="  p-3">
+            <img src="/icons/instagram.png" className="" />
+          </a>
+        </div>
       </div>
     </footer>
   );
